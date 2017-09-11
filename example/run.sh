5e14bc8c947a49df4caf0fadad8a013b8c535106
Matthew R. Becker <beckermr@users.noreply.github.com>
2017-09-11 11:21:39 -0500

    Update run.sh

--- example/run.sh
+++ example/run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-echo "PLATFORM_OBJECT_ID=1089
+echo "PLATFORM_OBJECT_ID=<NOTEBOOK ID>
 CIVIS_API_KEY=${CIVIS_API_KEY}" > my.env
 
 exec docker run --rm -p 8888:8888 --env-file my.env py3
