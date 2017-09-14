9705aa6fc75335e67c49b7c0f59ec1f41e407b02
Matthew R. Becker <beckermr@users.noreply.github.com>
2017-09-14 16:37:05 -0500

    DOC typo

--- civis_jupyter_notebooks/assets/jupyter_notebook_config.py
+++ civis_jupyter_notebooks/assets/jupyter_notebook_config.py
@@ -1,6 +1,6 @@
 """Jupyter configuration file
 
-Note that the code in this file gets executed in a python process that it is independent
+Note that the code in this file gets executed in a python process that is independent
 of any python process in a kernel run by Jupyter (e.g., an ipython kernel). Thus we can make
 changes to the installed packages to use in the ipython kernel here without worrying about
 them not being reimported.
