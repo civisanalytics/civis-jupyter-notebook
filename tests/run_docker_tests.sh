1749f476a658fed3e528265cfe750dac9a11bce0
Matthew R. Becker <beckermr@users.noreply.github.com>
2017-09-14 11:44:54 -0500

    TST make sure tests fail if something does not work

--- tests/run_docker_tests.sh
+++ tests/run_docker_tests.sh
@@ -1,9 +1,18 @@
 #!/bin/bash
 
-set -e
+set +e
 
-cp $1 .
-docker build -t img .
-docker run img echo "BUILDS OK"
-docker run img python -c "import civis"
-docker run img ipython -c "%load_ext civis_jupyter_ext"
+(
+    set -e
+    cp $1 .
+    docker build -t img .
+    docker run img echo "BUILDS OK"
+    docker run img python -c "import civis"
+    docker run img ipython -c "%load_ext civis_jupyter_ext"
+    docker run img ipython -c "import funny; print(funny.printstr())"
+)
+
+SUCCESS=$?
+if [ $SUCCESS -ne 0 ]; then
+    exit 1;
+fi
