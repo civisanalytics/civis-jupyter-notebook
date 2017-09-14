6a52dd3fb48d680a3a0ecd8edc5f51ab3bf8a8ee
beckermr <mbecker@civisanalytics.com>
2017-09-14 01:01:19 -0500

    DEP don't use an env file

--- tests/run_dev_image.sh
+++ tests/run_dev_image.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
 # the first argument to the script needs to be a Civis Platform notebook ID
-echo "PLATFORM_OBJECT_ID=$1
-CIVIS_API_KEY=${CIVIS_API_KEY}" > my.env
-
-docker run --rm -p 8888:8888 --env-file my.env py3
-
-rm my.env
+docker run --rm -p 8888:8888 \
+-e PLATFORM_OBJECT_ID=$1 -e CIVIS_API_KEY=${CIVIS_API_KEY} py3
