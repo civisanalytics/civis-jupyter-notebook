b4b77fd2ca0665436455b76edcc911c5c296c251
Matthew R. Becker <beckermr@users.noreply.github.com>
2017-09-14 11:36:24 -0500

    TST remove tests that no longer works

--- tests/run_docker_tests.sh
+++ tests/run_docker_tests.sh
@@ -7,4 +7,3 @@ docker build -t img .
 docker run img echo "BUILDS OK"
 docker run img python -c "import civis"
 docker run img ipython -c "%load_ext civis_jupyter_ext"
-docker run img ipython -c "import funny; funny.printstr()"
