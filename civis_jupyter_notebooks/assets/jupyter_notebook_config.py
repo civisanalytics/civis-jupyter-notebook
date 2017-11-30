18ba201e53f4350488c59f75a165157ebc4397dc
Salil Gupta <sgupta@civisanalytics.com>
2017-11-30 00:05:39 -0600

    setting redirect based on git installation

--- civis_jupyter_notebooks/assets/jupyter_notebook_config.py
+++ civis_jupyter_notebooks/assets/jupyter_notebook_config.py
@@ -21,34 +21,36 @@ c.NotebookApp.token = ''
 c.NotebookApp.tornado_settings = {'headers': {'Content-Security-Policy': "frame-ancestors *"}}
 c.MultiKernelManager.default_kernel_name = os.environ['DEFAULT_KERNEL']
 c.NotebookApp.allow_root = True
+c.NotebookApp.default_url = '/notebooks/notebook.ipynb'
 c.FileContentsManager.post_save_hook = platform_persistence.post_save
 
-# if error log has entries ignore extra set up
 if log_utils.log_file_has_logs(log_utils.USER_LOGS_FILE):
     # redirect to log file
-    return # NOQA
+    c.NotebookApp.default_url = '/edit/logs/errors.log'
+    platform_persistence.logger.info('user visible error log file has an entry')
+else:
+    NOTEBOOK_PATH = os.path.expanduser(os.path.join('~', 'work', 'notebook.ipynb'))
+    nb_file_path = os.environ.get('NOTEBOOK_FILE_PATH')
+    if nb_file_path:
+        NOTEBOOK_PATH = os.path.join(os.environ.get('GIT_REPO_MOUNT_PATH'), nb_file_path.strip('/'))
+        c.NotebookApp.default_url = os.path.join('/notebooks', 'repo', nb_file_path.strip('/'))
 
-NOTEBOOK_PATH = os.path.expanduser(os.path.join('~', 'work', 'notebook.ipynb'))
-nb_file_path = os.environ.get('NOTEBOOK_FILE_PATH')
-if nb_file_path:
-    NOTEBOOK_PATH = os.path.join(os.environ.get('GIT_REPO_MOUNT_PATH'), nb_file_path.strip('/'))
+    # pull .ipynb file from s3 and save preview back if necessary
+    # pull requirements.txt file from s3
+    try:
+        if not os.path.isfile(NOTEBOOK_PATH):
+            platform_persistence.initialize_notebook_from_platform(NOTEBOOK_PATH)
 
-# pull .ipynb file from s3 and save preview back if necessary
-# pull requirements.txt file from s3
-try:
-    if not os.path.isfile(NOTEBOOK_PATH):
-        platform_persistence.initialize_notebook_from_platform(NOTEBOOK_PATH)
+        _, preview_url = platform_persistence.get_update_urls()
+        platform_persistence.generate_and_save_preview(preview_url, NOTEBOOK_PATH)
+    except NotebookManagementError as e:
+        platform_persistence.logger.error(str(e))
+        platform_persistence.logger.warn('Killing the notebook process b/c of a startup issue')
+        os.kill(os.getpid(), signal.SIGTERM)
 
-    _, preview_url = platform_persistence.get_update_urls()
-    platform_persistence.generate_and_save_preview(preview_url, NOTEBOOK_PATH)
-except NotebookManagementError as e:
-    platform_persistence.logger.error(str(e))
-    platform_persistence.logger.warn('Killing the notebook process b/c of a startup issue')
-    os.kill(os.getpid(), signal.SIGTERM)
-
-# Install requirements.txt
-REQUIREMENTS_PATH = os.path.expanduser(os.path.join('~', 'work', 'requirements.txt'))
-if os.path.isfile(REQUIREMENTS_PATH):
-    platform_persistence.logger.info('installing requirements.txt packages')
-    pip.main(['install', '-r', REQUIREMENTS_PATH])
-    platform_persistence.logger.info('requirements.txt installed')
+    # Install requirements.txt
+    REQUIREMENTS_PATH = os.path.expanduser(os.path.join('~', 'work', 'requirements.txt'))
+    if os.path.isfile(REQUIREMENTS_PATH):
+        platform_persistence.logger.info('installing requirements.txt packages')
+        pip.main(['install', '-r', REQUIREMENTS_PATH])
+        platform_persistence.logger.info('requirements.txt installed')
