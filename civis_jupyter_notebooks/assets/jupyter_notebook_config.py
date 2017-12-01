77d4e28df9d8ea38fcb9d8ead29c5e1b6107f604
Salil Gupta <sgupta@civisanalytics.com>
2017-12-01 13:24:00 -0600

    DRYing the code

--- civis_jupyter_notebooks/assets/jupyter_notebook_config.py
+++ civis_jupyter_notebooks/assets/jupyter_notebook_config.py
@@ -23,32 +23,24 @@ c.NotebookApp.allow_root = True
 c.FileContentsManager.post_save_hook = platform_persistence.post_save
 c.MultiKernelManager.default_kernel_name = os.environ['DEFAULT_KERNEL']
 
-NOTEBOOK_PATH = os.path.expanduser(os.path.join('~', 'work'))
-REQUIREMENTS_PATH = os.path.expanduser(os.path.join('~', 'work'))
+ROOT_DIR = os.path.expanduser(os.path.join('~', 'work'))
 
 if log_utils.log_file_has_logs(log_utils.USER_VISIBLE_LOGS):
     # redirect to log file
     c.NotebookApp.default_url = '/edit/civis-notebook-logs.log'
-    platform_persistence.logger.info('user visible error log file has an entry')
-
 else:
-    nb_file_path = os.environ.get('NOTEBOOK_FILE_PATH')
-    if nb_file_path:
-        nb_file_path = nb_file_path.strip('/')
-        NOTEBOOK_PATH = os.path.join(NOTEBOOK_PATH, nb_file_path)
-        c.NotebookApp.default_url = '/notebooks/{}'.format(nb_file_path)
-    else:
-        NOTEBOOK_PATH = os.path.join(NOTEBOOK_PATH, 'notebook.ipynb')
-        c.NotebookApp.default_url = '/notebooks/notebook.ipynb'
+    nb_file_path = os.environ.get('NOTEBOOK_FILE_PATH', 'notebook.ipynb').strip('/')
+    notebook_full_path = os.path.join(ROOT_DIR, nb_file_path)
+    c.NotebookApp.default_url = '/notebooks/{}'.format(nb_file_path)
 
     try:
         # pull .ipynb file from s3 and save preview back if necessary
         # pull requirements.txt file from s3
-        if not os.path.isfile(NOTEBOOK_PATH):
-            platform_persistence.initialize_notebook_from_platform(NOTEBOOK_PATH)
+        if not os.path.isfile(notebook_full_path):
+            platform_persistence.initialize_notebook_from_platform(notebook_full_path)
 
         _, preview_url = platform_persistence.get_update_urls()
-        platform_persistence.generate_and_save_preview(preview_url, NOTEBOOK_PATH)
+        platform_persistence.generate_and_save_preview(preview_url, notebook_full_path)
 
     except NotebookManagementError as e:
         platform_persistence.logger.error(str(e))
@@ -56,7 +48,7 @@ else:
         os.kill(os.getpid(), signal.SIGTERM)
 
     # Install requirements.txt
-    REQUIREMENTS_PATH = os.path.join(REQUIREMENTS_PATH, 'requirements.txt')
+    REQUIREMENTS_PATH = os.path.join(ROOT_DIR, 'requirements.txt')
     if os.path.isfile(REQUIREMENTS_PATH):
         platform_persistence.logger.info('installing requirements.txt packages')
         pip.main(['install', '-r', REQUIREMENTS_PATH])
