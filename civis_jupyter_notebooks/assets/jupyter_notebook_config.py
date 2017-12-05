8bad5a74bc2a00d9996dd35585e83be5b8c01f4f
Matt Brennan <mbrennan@civisanalytics.com>
2017-12-05 11:42:47 -0500

    remove duplicate config settings

--- civis_jupyter_notebooks/assets/jupyter_notebook_config.py
+++ civis_jupyter_notebooks/assets/jupyter_notebook_config.py
@@ -26,8 +26,6 @@ c.NotebookApp.open_browser = False
 c.NotebookApp.token = ''
 c.NotebookApp.tornado_settings = {'headers': {'Content-Security-Policy': "frame-ancestors *"}}
 c.NotebookApp.terminado_settings = {'shell_command': ['bash']}
-c.NotebookApp.allow_root = True
-c.FileContentsManager.post_save_hook = platform_persistence.post_save
 c.MultiKernelManager.default_kernel_name = os.environ['DEFAULT_KERNEL']
 c.NotebookApp.allow_root = True
 
