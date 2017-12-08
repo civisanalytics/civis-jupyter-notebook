c1e082f9e6a9a7c1606554dc016f2de69b0fc4cc
Matthew Brennan <matty.brennan@gmail.com>
2017-12-08 16:28:25 -0500

    Support requirements.txt from git repos
    * Traverse directory for requirements.txt
    
    * print pip output as string
    
    * remove fancy default_path setting
    
    * overwrite requirements if url provided
    
    * fix tests
    
    * refactor config
    
    * refactor git requirements install
    
    * fix check for notebook/terminal
    
    * pip: break up function, don't install from /root
    
    * requirements: add specs
    
    * requirements: use file_logger for errors
    
    * better jupyter_notebook_config testing
    
    * specify function parameters in config tests
    
    * use magicmock for consistency

--- civis_jupyter_notebooks/assets/jupyter_notebook_config.py
+++ civis_jupyter_notebooks/assets/jupyter_notebook_config.py
@@ -5,46 +5,7 @@ of any python process in a kernel run by Jupyter (e.g., an ipython kernel). Thus
 changes to the installed packages to use in the ipython kernel here without worrying about
 them not being reimported.
 """
-import os
-import signal
-import pip
-from civis_jupyter_notebooks import platform_persistence, log_utils
-from civis_jupyter_notebooks.platform_persistence import NotebookManagementError
+from civis_jupyter_notebooks import notebook_config
 
-# Jupyter Configuration
 c = get_config() # noqa
-c.NotebookApp.ip = '*'
-c.NotebookApp.allow_origin = '*'
-c.NotebookApp.port = 8888
-c.NotebookApp.open_browser = False
-c.NotebookApp.token = ''
-c.NotebookApp.tornado_settings = {'headers': {'Content-Security-Policy': "frame-ancestors *"}}
-c.NotebookApp.terminado_settings = {'shell_command': ['bash']}
-c.MultiKernelManager.default_kernel_name = os.environ['DEFAULT_KERNEL']
-c.NotebookApp.allow_root = True
-c.FileContentsManager.post_save_hook = platform_persistence.post_save
-
-ROOT_DIR = os.path.expanduser(os.path.join('~', 'work'))
-
-if log_utils.log_file_has_logs(log_utils.USER_VISIBLE_LOGS):
-    # redirect to log file
-    c.NotebookApp.default_url = '/edit/civis-notebook-logs.log'
-else:
-    nb_file_path = os.environ.get('NOTEBOOK_FILE_PATH', 'notebook.ipynb').strip('/')
-    notebook_full_path = os.path.join(ROOT_DIR, nb_file_path)
-    c.NotebookApp.default_url = '/notebooks/{}'.format(nb_file_path)
-
-    try:
-        platform_persistence.initialize_notebook_from_platform(notebook_full_path)
-        platform_persistence.post_save({'type': 'notebook'}, notebook_full_path, None)
-    except NotebookManagementError as e:
-        platform_persistence.logger.error(str(e))
-        platform_persistence.logger.warn('Killing the notebook process b/c of a startup issue')
-        os.kill(os.getpid(), signal.SIGTERM)
-
-    # Install requirements.txt
-    REQUIREMENTS_PATH = os.path.join(ROOT_DIR, 'requirements.txt')
-    if os.path.isfile(REQUIREMENTS_PATH):
-        platform_persistence.logger.info('installing requirements.txt packages')
-        pip.main(['install', '-r', REQUIREMENTS_PATH])
-        platform_persistence.logger.info('requirements.txt installed')
+notebook_config.civis_setup(c)
