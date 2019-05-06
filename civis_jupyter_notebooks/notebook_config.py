88866aab1ab64d3069325c6591ffb28c03d1c18f
Leanne Miller <lmiller@civisanalytics.com>
2019-05-06 13:04:13 -0500

    Set log_level on the Application attribute

--- civis_jupyter_notebooks/notebook_config.py
+++ civis_jupyter_notebooks/notebook_config.py
@@ -41,12 +41,12 @@ def config_jupyter(c):
     c.NotebookApp.tornado_settings = {'headers': {'Content-Security-Policy': "frame-ancestors *"}}
     c.NotebookApp.terminado_settings = {'shell_command': ['bash']}
     c.NotebookApp.allow_root = True
-    c.NotebookApp.log_level = 'INFO'
     c.NotebookApp.nbserver_extensions = {
         'civis_jupyter_notebooks.extensions.git.uncommitted_changes': True
     }
     c.FileContentsManager.post_save_hook = platform_persistence.post_save
     c.MultiKernelManager.default_kernel_name = os.environ['DEFAULT_KERNEL']
+    c.Application.log_level = 'INFO'
 
 
 def stage_new_notebook(notebook_file_path):
