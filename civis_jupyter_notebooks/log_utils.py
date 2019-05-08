2724953aa018a68f3912e12b6243cad870ab2690
Leanne Miller <lmiller@civisanalytics.com>
2019-05-08 16:13:23 -0500

    Improve log util method documentation

--- civis_jupyter_notebooks/log_utils.py
+++ civis_jupyter_notebooks/log_utils.py
@@ -17,28 +17,38 @@ class SingleLevelFilter(logging.Filter):
 
 
 def setup_stream_logging():
-    """ Set up logging"""
+    """
+    Configure the CIVIS_PLATFORM_BACKEND logger for stream logging.
+
+    DEBUG level logs will be ignored,
+    INFO level logs will be sent to stdout,
+    WARNING, ERROR, & CRITICAL level logs will be sent to stderr.
+
+    Returns
+    -------
+    The CIVIS_PLATFORM_BACKEND logger
+    """
     logger = logging.getLogger('CIVIS_PLATFORM_BACKEND')
 
-    # prevents duplicate log records by preventing messages from being passed to ancestor loggers, i.e. the root logger
+    # prevents duplicate log records by preventing messages from being passed to ancestor loggers, e.g. the root logger
     logger.propagate = False
 
-    # Sets the lowest level which this logger will pay attention to (i.e. debug level logs will be ignored)
+    # Sets the lowest level which this logger will pay attention to (i.e. ignore debug level logs)
     logger.setLevel(logging.INFO)
 
     formatter = logging.Formatter(
         fmt='[%(levelname).1s %(asctime)s %(name)s] %(message)s',
         datefmt="%H:%M:%S")
 
-    # Configures a handler for sending warning level and above messages to stderr
+    # Configures a handler for sending warning level and above logs to stderr
     error_handler = logging.StreamHandler(stream=sys.__stderr__)
     error_handler.setLevel(logging.WARNING)
     error_handler.setFormatter(formatter)
     logger.addHandler(error_handler)
 
-    # Configures a handler for sending info level and above messages to stdout
+    # Configures a handler for sending info level logs to stdout
     info_handler = logging.StreamHandler(stream=sys.__stdout__)
-    # info_handler.setLevel(logging.INFO)
+    info_handler.setLevel(logging.INFO)
     # Filter so that the info handler will handle ONLY info level logs
     info_handler.addFilter(SingleLevelFilter(logging.INFO))
     info_handler.setFormatter(formatter)
@@ -55,12 +65,21 @@ def setup_stream_logging():
     return logger
 
 
-# File logging is intended for errors to be shown to the user.
-# E.g. If we fail to install requirements, then we redirect
-# c.NotebookApp.default_url to point to this log file
-# TODO: This was written during the intial development of
-# notebooks and may no longer be necessary. CIVP-18444
 def setup_file_logging():
+    """
+    Configure the USER_VISIBLE_LOGS for file logging.
+
+    File logging is intended for errors to be shown to the user.
+    E.g. If we fail to install requirements, then we redirect
+    c.NotebookApp.default_url to point to this log file.
+
+    TODO: This was written during the intial development
+    of notebooks and may no longer be necessary. CIVP-18444
+
+    Returns
+    -------
+    The USER_VISIBLE_LOGS logger
+    """
     directory = os.path.dirname(USER_VISIBLE_LOGS)
     if not os.path.exists(directory):
         os.makedirs(directory)
@@ -79,11 +98,6 @@ def setup_file_logging():
     handler.setFormatter(formatter)
     logger.addHandler(handler)
 
-    logger.info('file logging info')
-    logger.warn('file logging warn')
-    logger.warning('file logging warning')
-    logger.error('file logging error!')
-    logger.critical('file logging critical!')
     return logger
 
 
