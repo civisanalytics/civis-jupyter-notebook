7a2aa04308dedb05ccd6de65720d6b153d2abe28
David Lewis <meng.lewis@gmail.com>
2020-01-30 12:15:42 -0600

    use token defaults

--- civis_jupyter_notebooks/notebook_config.py
+++ civis_jupyter_notebooks/notebook_config.py
@@ -32,7 +32,7 @@ def config_jupyter(c):
     c.NotebookApp.allow_origin = '*'
     c.NotebookApp.port = 8888
     c.NotebookApp.open_browser = False
-    c.NotebookApp.token = 'something'
+    c.NotebookApp.token = '<generated>'
     c.NotebookApp.tornado_settings = {'headers': {'Content-Security-Policy': "frame-ancestors *"}}
     c.NotebookApp.terminado_settings = {'shell_command': ['bash']}
     c.NotebookApp.allow_root = True
