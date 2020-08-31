097c2e48aff3e6e8d93e021ec69a2d527bf4c696
Mike Saelim <msaelim@civisanalytics.com>
2020-08-31 01:05:55 -0500

    remove deprecated resources='all' argument when instantiating the Civis API client

--- civis_jupyter_notebooks/tests/test_platform_persistence.py
+++ civis_jupyter_notebooks/tests/test_platform_persistence.py
@@ -185,7 +185,7 @@ class PlatformPersistenceTest(unittest.TestCase):
     @patch('civis.APIClient')
     def test_will_regenerate_api_client(self, mock_client):
         platform_persistence.get_client()
-        mock_client.assert_called_with(resources='all')
+        mock_client.assert_called()
 
     @patch('os.path.isfile')
     @patch('os.path.isdir')
