70303068e2bfc3b68bd6b931b245b9c440cb92ca
Jackson Lee <86482098+jacksonlee-civis@users.noreply.github.com>
2024-06-11 12:47:50 -0400

    [CIVIS-2326] MAINT update repo setup and packaging (#57)
    * MAINT switch to pyproject.toml, add support for python 3.12, drop python 3.8
    
    * MAINT setup_remote_docker
    
    * MAINT update ci config and add github templates
    
    * FIX include dotfiles in setuptools.package-data
    
    * REF refactor legacy python code

--- civis_jupyter_notebooks/__init__.py
+++
@@ -1,8 +0,0 @@
-try:
-    from importlib.metadata import version
-except ModuleNotFoundError:
-    # For Python 3.7
-    from importlib_metadata import version
-
-
-__version__ = version("civis-jupyter-notebook")
--- src/civis_jupyter_notebooks/extensions/git/uncommitted_changes.py
+++ src/civis_jupyter_notebooks/extensions/git/uncommitted_changes.py
@@ -13,7 +13,7 @@ class UncommittedChangesHandler(RequestHandler):
         civis_git = CivisGit()
 
         if not civis_git.is_git_enabled():
-            self.set_status(404, 'Not a git enabled notebook')
+            self.set_status(404, "Not a git enabled notebook")
         else:
             has_changes = False
             try:
@@ -21,7 +21,7 @@ class UncommittedChangesHandler(RequestHandler):
             except CivisGitError:
                 pass
 
-            response['dirty'] = has_changes
+            response["dirty"] = has_changes
             self.set_status(200)
         self.finish(response)
 
@@ -32,10 +32,13 @@ def log_func(handler):
 
 
 def load_jupyter_server_extension(nbapp):
-    nbapp.log.info('Uncommitted Changes Ext. Loaded')
+    nbapp.log.info("Uncommitted Changes Ext. Loaded")
 
     webapp = nbapp.web_app
-    base_url = webapp.settings['base_url']
-    webapp.add_handlers(".*$", [
-        (ujoin(base_url, r"/git/uncommitted_changes"), UncommittedChangesHandler),
-    ])
+    base_url = webapp.settings["base_url"]
+    webapp.add_handlers(
+        ".*$",
+        [
+            (ujoin(base_url, r"/git/uncommitted_changes"), UncommittedChangesHandler),
+        ],
+    )
