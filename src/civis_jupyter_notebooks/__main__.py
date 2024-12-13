e22867c44eb8b0ae2dc810ddc7a0e7d0e91ae2d6
Casey Conlin <casey.conlin@gmail.com>
2024-12-13 09:13:34 -0500

    Upate deps, add back config settings.

--- src/civis_jupyter_notebooks/__main__.py
+++ src/civis_jupyter_notebooks/__main__.py
@@ -60,8 +60,12 @@ def cli():
     for fe_ext in frontend_extensions:
         _copy(("assets", "extensions", fe_ext), ("~", ".jupyter", "extensions"))
     subprocess.check_call(
-        "pip install /civis-jupyter-notebook/src/civis_jupyter_notebooks/assets/extensions/civis_jupyter_notebook_extensions-0.1.0-py3-none-any.whl", 
-        shell=True
+        (
+            "pip install "
+            "/civis-jupyter-notebook/src/civis_jupyter_notebooks/assets/extensions/"  # fmt: skip
+            "civis_jupyter_notebook_extensions-0.1.0-py3-none-any.whl"
+        ),
+        shell=True,
     )  # nosec
     # install and enable nbextensions
     # subprocess.check_call(
