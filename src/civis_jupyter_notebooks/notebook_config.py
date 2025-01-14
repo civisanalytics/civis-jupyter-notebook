67be559fe1a4b0337fbc49540278d4e03c3db1d2
Casey Conlin <casey.conlin@gmail.com>
2025-01-14 18:08:03 -0500

    Update config and start.

--- src/civis_jupyter_notebooks/notebook_config.py
+++ src/civis_jupyter_notebooks/notebook_config.py
@@ -73,7 +73,7 @@ def civis_setup(c):
     # c.NotebookApp.default_url = "/notebooks/{}".format(nb_file_path)
     # c.ServerApp.default_url = "/notebooks/{}".format(nb_file_path)
     # c.ServerApp.default_url = "/doc/workspaces/auto-s/tree/{}".format(nb_file_path)
-    c.ServerApp.default_url = format(nb_file_path)
+    # c.LabApp.default_url = "/lab/tree/{}".format(nb_file_path)
     get_notebook(notebook_full_path)
     stage_new_notebook(nb_file_path)
 
